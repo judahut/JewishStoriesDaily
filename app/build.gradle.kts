ae3907c5a201814d3fd8b71504500968f3a7b58e
Yehuda Greenfield <greenfieldyehuda@gmail.com>
2025-12-23 22:58:14 -0500

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.Greenfield.DailyTalmud"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
