e6ac88bbc6df606862c9eadf19e32424df0dd312
Yehuda Greenfield <greenfieldyehuda@gmail.com>
2025-12-25 11:23:59 -0500

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.Greenfield.DailyTalmud"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
